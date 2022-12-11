1b19b3838a8caaf3c0baca2d6d3fb0ebf41a3766
omar1779 <josemyclans@gmail.com>
2022-12-11 00:11:01 -0500

    move img in home

--- src/components/Bienvenida.jsx
+++ src/components/Bienvenida.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import logo from "../assets/home.gif"
 function Bienvenida() {
   return (
     <div className="flex h-full flex-col lg:flex-row items-center justify-around lg:pl-10 text-center 3xl:pr-20">
-      <img src="../assets/gif home.gif"
+      <img src={logo}
 			className="h-80 w-80 md:h-96 md:w-96 lg:h-64 lg:w-64 3xl:h-3/5 3xl:w-3/5"
-			alt="woman" />
+			alt="gif de bienvenida" />
       <div className="flex flex-col justify-around pl-9 pr-9 3xl:h-3/5">
         <Link to="/products">
           <h1 href="#" className="font-thin text-violet-900 text-4xl mb-5 md:text-7xl md:mb-10 lg:text-5xl 3xl:text-8xl animate-pulse">
